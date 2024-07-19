7fc19ceccc7eb4af0a0bc5d79845ee743048866b
bayu.adinata <bayu.adinata@mindigitalgroup.com>
2024-07-19 23:30:17 +0700

    first commit

---
+++ src/main/java/com/uph/replication/core/replication_core/ReplicationCoreApplication.java
@@ -0,0 +1,13 @@
+package com.uph.replication.core.replication_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplicationCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uph/replication/core/replication_core/ReplicationCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uph.replication.core.replication_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicationCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
