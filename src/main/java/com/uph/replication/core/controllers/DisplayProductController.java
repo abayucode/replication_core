805a054cbc11e3bd8b13ec4bdb5b71fa23bf1815
abayucode <adinatabayu760@gmail.com>
2024-10-06 14:28:29 +0700

    [improve][delete unused]

--- src/main/java/com/uph/replication/core/controllers/DisplayProductController.java
+++ src/main/java/com/uph/replication/core/controllers/DisplayProductController.java
@@ -4,7 +4,6 @@ import com.uph.replication.core.constants.APIConstants;
 import com.uph.replication.core.dto.ApiResult;
 import com.uph.replication.core.dto.requests.ReqSearchProduct;
 import com.uph.replication.core.dto.responses.RespDisplayProductsDTO;
-import com.uph.replication.core.entities.MasterProducts;
 import com.uph.replication.core.entities.SetProductsStore;
 import com.uph.replication.core.services.DisplayProductStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(APIConstants.B2C_BASE_PRODUCT_URL)
+@CrossOrigin
 public class DisplayProductController {
 
     @Autowired
